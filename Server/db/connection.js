dfcd8c99d41c7783f39fffd1697921efc5897edf
Coder-Towhid <islamsmtowhidul@gmail.com>
2023-12-16 01:33:55 +0600

    db server added

--- Server/db/connection.js
+++ Server/db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 console.log("server",process.env.Db_Username)
 const connection = mysql.createConnection({
-    host: process.env.Server,
+    host: process.env.Db_Server,
     user: process.env.Db_Username,
     password: process.env.Db_Password,
     database: process.env.Database,
